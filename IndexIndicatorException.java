f5c591eeab6cb7802894cfa24b13389a22324359
Konstantinos Paganopoulos <k.paganopoulos@gmail.com>
2020-02-25 00:52:05 +0000

    Create IndexIndicatorException.java

---
+++ IndexIndicatorException.java
@@ -0,0 +1,13 @@
+/*
+ * IndexIndicatorException
+ *
+ * Author: Lasse Collin <lasse.collin@tukaani.org>
+ *
+ * This file has been put into the public domain.
+ * You can do whatever you want with this file.
+ */
+
+
+class IndexIndicatorException extends Exception {
+    private static final long serialVersionUID = 1L;
+}

3aba1cb706f026311f42c14a0fdace9946f9890e
Konstantinos Paganopoulos <k.paganopoulos@gmail.com>
2020-02-25 00:51:44 +0000

    Create ComLZW.java

---
+++ ComLZW.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.List;
+
+
+public  class ComLZW {
+
+/** Compress a string to a list of output symbols. */
+    public static List<Integer> compress(String uncompressed) {
+        // Build the dictionary.
+        int dictSize = 256;
+        Map<String,Integer> dictionary = new HashMap<String,Integer>();
+        for (int i = 0; i < 256; i++)
+            dictionary.put("" + (char)i, i);
+
+        String w = "";
+        List<Integer> result = new ArrayList<Integer>();
+        for (char c : uncompressed.toCharArray()) {
+            String wc = w + c;
+            if (dictionary.containsKey(wc))
+                w = wc;
+            else {
+                result.add(dictionary.get(w));
+                // Add wc to the dictionary.
+                dictionary.put(wc, dictSize++);
+                w = "" + c;
+            }
+        }
+
+        // Output the code for w.
+        if (!w.equals(""))
+            result.add(dictionary.get(w));
+        return result;
+    }
+}

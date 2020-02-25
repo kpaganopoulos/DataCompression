7e2aa10b95d79954c325e86ada0819be3515dc0f
Konstantinos Paganopoulos <k.paganopoulos@gmail.com>
2020-02-25 00:53:17 +0000

    Create XZFormatException.java

---
+++ XZFormatException.java
@@ -0,0 +1,23 @@
+/*
+ * XZFormatException
+ *
+ * Author: Lasse Collin <lasse.collin@tukaani.org>
+ *
+ * This file has been put into the public domain.
+ * You can do whatever you want with this file.
+ */
+
+
+/**
+ * Thrown when the input data is not in the XZ format.
+ */
+public class XZFormatException extends XZIOException {
+    private static final long serialVersionUID = 3L;
+
+    /**
+     * Creates a new exception with the default error detail message.
+     */
+    public XZFormatException() {
+        super("Input is not in the XZ format");
+    }
+}
